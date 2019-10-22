37643ec6179fc5e790523d5dd50908db3cc8d223
zhoukaiyuan <zhoukaiyuan@ulord.net>
2019-10-22 15:53:23 +0800

    add a file for learn descriptor

--- basic_learn/class_descriptor/mydescriptor.py
+++ basic_learn/class_descriptor/mydescriptor.py
@@ -24,6 +24,38 @@ class test1(object):
     name = normal("")
 
 
+class Score:
+    def __init__(self,default):
+        self._score = default
+    
+    def __set__(self,instance,value):
+        if not isinstance(value,int):
+            raise TypeError('Score must be integer')
+        if not 0 <= value <= 100:
+            raise ValueError("Valid value must be in [0,100]")
+        self._score = value
+
+    def __get__(self,instance,owner):
+        return self._score
+
+    def __del__(self):
+        del self._score
+
+class Student:
+    math = Score(0)
+    chinese = Score(0)
+    english = Score(0)
+
+    def __init__(self,name,math,chinese,english):
+        self.name=name
+        self.math=math
+        self.chinese = chinese
+        self.english = english
+    
+    def __repr__(self):
+        return "<Student: {}, math:{}, chinese: {}, english:{}>".format(self.name,\
+            self.math,self.chinese,self.english)
+
 def main():
     t = test()
     print(t.name)
@@ -38,5 +70,10 @@ def main():
     t1.name = 3
     print(t1.name)
 
+    print("++++++++++++++++++++++++++++++")
+    std1 = Student('小明',7,837,68)
+    print(std1)
+    std2 = Student('小明','76','87','68')
+    print(std2)
 if __name__ == '__main__':
     main()
