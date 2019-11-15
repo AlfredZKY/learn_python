f4af9d3ec1eeaa6f2d56ada24640329a7ac162e3
zhoukaiyuan <zhoukaiyuan@ulord.net>
2019-11-15 16:17:49 +0800

    update code

--- 小技巧/dataclass.py
+++ 小技巧/dataclass.py
@@ -1,8 +1,17 @@
 from collections import namedtuple
-
+from dataclasses import dataclass
 # 以前简单的类可以使用namedtuple实现
 Car = namedtuple('Car','color mileage')
 
-my_car = Car('red','3812.4','Auto')
+my_car = Car('red',3812.4)
+print(my_car.color)
+print(my_car)
+
+@dataclass
+class Car1:
+    color:str
+    mileage:float
+
+my_car = Car1('red',3812.4)
 print(my_car.color)
 print(my_car)
