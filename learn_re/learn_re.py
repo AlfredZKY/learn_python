037b14bba09605747170757576ed45fec5750dc0
zky <zky@zkydeiMac.local>
2020-04-16 22:42:30 +0800

    add a file for learn process and threading

---
+++ learn_re/learn_re.py
@@ -0,0 +1,12 @@
+import re
+
+content = 'Hello 123 4567 World_This is a Regex Demo'
+content = 'Hello 1234567 World_This is a Regex Demo'
+print(len(content))
+
+# result = re.match('^Hello\s\d\d\d\s\d{4}\s\w{10}',content)
+result = re.match('^Hello\s(\d+)\sWorld',content)
+print(result)
+print(result.group())
+print(result.group(1))
+print(result.span)
