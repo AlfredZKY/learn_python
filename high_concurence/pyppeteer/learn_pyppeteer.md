5005f87b9a159ad620e5a10283c60cada487264e
zky <zky@zkydeiMac.local>
2020-05-01 16:23:15 +0800

    Perfect the learning of pyppeteer

--- high_concurence/pyppeteer/learn_pyppeteer.md
+++ high_concurence/pyppeteer/learn_pyppeteer.md
@@ -17,3 +17,39 @@ pyppeteer.launcher.launch(options: dict = None, **kwargs) → pyppeteer.browser.
     - logLevel  (int|str)：日志级别，默认和 root logger 对象的级别相同。
     - autoClose (bool)：当一些命令执行完之后，是否自动关闭浏览器，默认是 True。
     - loop (asyncio.AbstractEventLoop)：事件循环对象。
+
+# Browser
+    luanch()方法返回的就是Browser对象，即浏览器对象，我们会通常将其赋值给browser变量
+    class pyppeteer.browser.Browser(connection: pyppeteer.connection.Connection, contextIds: List[str], ignoreHTTPSErrors: bool, 
+    setDefaultViewport: bool, process: Optional[subprocess.Popen] = None, closeCallback: Callable[[], Awaitable[None]] = None, **kwargs)
+    通过设置浏览器参数就可修改浏览器的配置
+
+# Page
+    Page即页面，就对应一个网页，一个选项卡
+        - 选择器
+            Page对象内置了一些用于选取节点的选择器方法，如J方法传入一个选择器Selector,则能返回对应匹配的第一个节点，等价于querySelector,
+            如JJ方法则是返回符合Selector的列表，类似于querySelectorAll
+        
+        - 选项卡操作
+            newpage()即创建选项卡操作
+
+# pyppeteer模拟操作
+    - click点击
+        button 鼠标按钮，分为left middle right
+        clickcount 点击次数，如双击，单击
+        delay 延迟点击
+        输入文本
+    
+    - 输入文本
+        page.type() 传入两个函数，一个是选择器，一个是要输入的文本
+
+# 让页面等待符合条件的节点加载出来再返回
+    - 在这里waitForSelector就是传入一个CSS选择器，如果找到了，立马返回结果，否则等待直到超时
+    - 除了waitForSelector方法，还有很多其他的等待方法：
+        - waitForFunction:等待某个Js方法执行完毕或返回结果
+        - waitForNavigation:等待页面跳转，如果没有加载出来就会报错
+        - waitForRequest:等待某个特定请求被发出
+        - waitForResponse:等待某个特定的请求收到了回应
+        - waitFor:等待通用的等待方法
+        - waitForSelector:等待符合选择器的节点加载出来
+        - waitForXPath:等待符合XPath的节点加载出来
--- high_concurence/pyppeteer/pyppeteer_headless.py
+++ high_concurence/pyppeteer/pyppeteer_headless.py
@@ -8,10 +8,19 @@
 
 import asyncio
 from pyppeteer import launch
+from pyquery import PyQuery as pq 
+
+proxy = '127.0.0.1:1087'
 
 url = 'https://www.baidu.com'
 
 check_url = 'https://antispider1.scrape.cuiqingcai.com/'
+check_url1 = 'https://antispider1.scrape.cuiqingcai.com/'
+check_url2 = 'https://antispider2.scrape.cuiqingcai.com/'
+
+click_url = 'https://dynamic2.scrape.cuiqingcai.com/'
+
+width,height = 1366,768
 
 async def headless_module():
     # 关闭无头模式，生产环境中还是关闭
@@ -26,7 +35,186 @@ async def dev_module():
     page = await browser.newPage()
     await page.goto(check_url)
     await asyncio.sleep(10)
-    
-# asyncio.get_event_loop().run_until_complete(headless_module())
-asyncio.get_event_loop().run_until_complete(dev_module())
 
+async def web_check():
+    # 设置浏览器窗口
+    browser = await launch(headless=False,args=['--disable-infobars',f'--window-size={width},{height}'])
+    page = await browser.newPage()
+    
+    # 设置浏览器页面的窗口
+    await page.setViewport({'width':width,'height':height})
+
+    # 执行js代码，过掉webdriver浏览器设置 
+    await page.evaluateOnNewDocument('Object.defineProperty(navigator,"webdriver",{get:()=>undefined})')
+    
+    await page.goto(check_url)
+    await asyncio.sleep(10)
+
+async def load_data():
+    url = 'https://www.taobao.com'
+    browser = await launch(headless=False,userDataDir='high_concurence/pyppeteer/userdata',args=['--disable-infobars',f'--window-size={width},{height}'])
+    page = await browser.newPage()
+
+    # 设置页面窗口
+    await page.setViewport({'width':width,'height':height})
+
+    await page.goto(url)
+    await asyncio.sleep(100)
+
+async def clear_data():
+    browser = await launch(headless=False,args=['--window-size={width},{height}','--disable-infobars'])
+
+    # 无痕模式
+    context = await browser.createIncognitoBrowserContext()
+    page = await context.newPage()
+
+    await page.setViewport({'width':width,'height':height})
+    await page.goto(url)
+    await asyncio.sleep(100)
+    await browser.close()
+
+
+async def page_selector():
+    browser = await launch()
+    page = await browser.newPage()
+
+    url = 'https://dynamic2.scrape.cuiqingcai.com/'
+
+    await page.goto(url)
+
+    await page.waitForSelector('.item .name')
+    j_result1 = await page.J('.item .name')
+    j_result2 = await page.querySelector('.item .name')
+    print(j_result1)
+    print(j_result2)
+
+    jj_result1 = await page.JJ('.item .name')
+    jj_result2 = await page.querySelectorAll('.item .name')
+
+    print(jj_result1)
+    print(jj_result2)
+
+    await asyncio.sleep(10)
+    await browser.close()
+
+async def page_switch():
+    browser = await launch(headless=False)
+
+    page = await browser.newPage()
+    await page.goto(url)
+
+    page = await browser.newPage()
+    await page.goto(url)
+
+    pages = await browser.pages()
+    print('Pages:',pages)
+    page1 = pages[1]
+
+    # 选项卡切换
+    await page1.bringToFront()
+
+    await asyncio.sleep(10)
+
+    await browser.close()
+
+# page Basic operation
+async def page_operate():
+    browser = await launch(headless=False,args=['--disable-infobars'])
+    page = await browser.newPage()
+
+    await page.goto(check_url1)
+    await page.goto(check_url2)
+
+    # 后退
+    await page.goBack()
+
+    # 前进
+    await page.goForward()
+
+    # 刷新
+    await page.reload()
+
+    # 保存
+    await page.pdf()
+
+    # 截图
+    await page.screenshot()
+
+    # 设置页面 HTML
+    await page.setContent('<h2>Hello World </h2>')
+
+    # 设置User-Agent
+    await page.setUserAgent('Python')
+
+    # 设置Headers
+    await page.setExtraHTTPHeaders(headers={})
+
+    await asyncio.sleep(2000)
+
+    # 关闭
+    await page.close()
+    await browser.close()
+
+async def click_operate():
+    browser = await launch(headless=False,args=['--disable-infobars'])
+    page = await browser.newPage()
+
+    await page.goto(click_url)
+    await page.waitForSelector('.item .name')
+    await page.click('.item .name',options={
+        'button':'right',
+        'clickCount':1,
+        'delay':3000,
+    })
+
+    await browser.close()
+
+async def input_text():
+    url = 'https://www.taobao.com'
+    browser = await launch(headless=False,args=['--disable-infobars',f'--window-size={width},{height}'])
+    page = await browser.newPage()
+
+    await page.goto(url)
+
+    # 设置窗口
+    await page.setViewport({'width':width,'height':height})
+
+    # 后退
+    await page.waitForSelector('#q')
+
+    await page.type('#q','iPad')
+
+    # 点击
+    await page.click('.btn-search', options={
+        'button':'left',
+        'clickCount':1,
+        'delay':300,
+        })
+
+    # 关闭
+    await asyncio.sleep(10)
+    await browser.close()
+
+
+async def get_info():
+    browser = await launch(headless=False,args=['--disable-infobars',f'--window-size={width},{height}'])
+
+    page = await browser.newPage()
+
+    await page.goto(click_url)
+    print('HTML:',await page.content())
+    print('Cookies:',await page.cookies())
+    await browser.close()
+
+
+# asyncio.get_event_loop().run_until_complete(headless_module())
+# asyncio.get_event_loop().run_until_complete(dev_module())
+# asyncio.get_event_loop().run_until_complete(web_check())
+# asyncio.get_event_loop().run_until_complete(load_data())
+# asyncio.get_event_loop().run_until_complete(clear_data())
+# asyncio.get_event_loop().run_until_complete(page_selector())
+# asyncio.get_event_loop().run_until_complete(page_switch())
+# asyncio.get_event_loop().run_until_complete(page_operate())
+# asyncio.get_event_loop().run_until_complete(click_operate())
+# asyncio.get_event_loop().run_until_complete(input_text())
+asyncio.get_event_loop().run_until_complete(get_info())
